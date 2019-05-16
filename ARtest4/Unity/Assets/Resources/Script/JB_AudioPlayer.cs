0f21bfe51336b59da2a621fbb4e0b1f086bad406
minjoonee <jbjb3044@gmail.com>
2019-05-16 16:14:00 +0900

    modify Unity TTs

---
+++ ARtest4/Unity/Assets/Resources/Script/JB_AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_AudioPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void SoundPlay()
+    {
+        this.audioSource.PlayOneShot(this.clip);
+        Debug.Log("음악파일 재생");
+    }
+}
---
+++ ARtest4/Unity/Assets/Resources/Script/filestream.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.IO;
+public class filestream : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    // Use this for initialization
+    string m_strPath = "Assets/Resources/";
+    
+    public void WriteData(string strData)
+    {
+        FileStream f = new FileStream(m_strPath + "Data.txt", FileMode.Append, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(f, System.Text.Encoding.Unicode);
+        writer.WriteLine(strData);
+        writer.Close();
+    }
+    
+    public void Parse()
+    {
+        TextAsset data = Resources.Load("Data", typeof(TextAsset)) as TextAsset;
+        StringReader sr = new StringReader(data.text);
+        
+        // 먼저 한줄을 읽는다. 
+        string source = sr.ReadLine();
+
+        string[] values;                // 쉼표로 구분된 데이터들을 저장할 배열 (values[0]이면 첫번째 데이터 )
+
+        while (source != null)
+        {
+            values = source.Split(',');  // 쉼표로 구분한다. 저장시에 쉼표로 구분하여 저장하였다.
+            if (values.Length == 0)
+            {
+                sr.Close();
+                return;
+            }
+            source = sr.ReadLine();    // 한줄 읽는다.
+        }
+    }
+
+
+
+    void Update()
+    { }
