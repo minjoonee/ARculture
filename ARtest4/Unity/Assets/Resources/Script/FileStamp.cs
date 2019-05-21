edd38e81aafc0cec49bbc6db36567068a2ea1878
minjoonee <jbjb3044@gmail.com>
2019-05-22 02:44:52 +0900

    Stamp
    파일입출력

---
+++ ARtest4/Unity/Assets/Resources/Script/FileStamp.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.IO;
+public class FileStamp : MonoBehaviour // 처음 실행했을 때 폴더 생성하는 역할이 되어줄 스크립트
+{
+    void Start() 
+    {
+        if (!Directory.Exists(Application.persistentDataPath + "/ARculture"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/ARculture");
+        }
+
+        FileStream fs = new FileStream(Application.persistentDataPath + "/ARculture" + "/TestFileName.txt", FileMode.Append, FileAccess.Write);
+
+        StreamWriter sw = new StreamWriter(fs);
+      
+        //경로 : 내 PC\Samsung Galaxy S7 edge\Phone\Android\data\com.AR.test\files
+        /* 
+         * 안드로이드에서도 이 경로를 불러와야 되는데 어떻게?
+         * 읽는건
+         * string str = string.Empty;
+         * StreamReader sr = new StreamReader(file);
+         * str = sr.ReadLine();
+         */
+        sw.WriteLine("write test\n");
+        sw.WriteLine("add write test"); // 한번에 추가되고 이스케이프 문자 활용 가능
+        sw.Flush();
+        sw.Close();
+        fs.Close();
+    }
+
+    void Update()
+    {
+
+    }
+}
--- ARtest4/Unity/Assets/Resources/Script/GameManager.cs
+++ ARtest4/Unity/Assets/Resources/Script/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Vuforia;
 using System.Xml;
+using System.IO;
 
 public class GameManager : TtsForm, ITrackableEventHandler
 {
@@ -13,15 +14,20 @@ public class GameManager : TtsForm, ITrackableEventHandler
     string filename;
     string jpg_filename;
     string str;
-
     int jpg_filename_idx;
-    int filename_idx=-1;
+    int filename_idx = -1;
     int count;
+    int list_count;
     private bool SoundTrackCount = false; // 인식했을 때 사용하는 변수
+    List<string> Stamp_List = new List<string>(); // 현재 스탬프에 저장되어 있는 데이터들(중복입력 방지 변수)
+
     Touch touch;
     // Start is called before the first frame update
     new void Start()
     {
+        Stamp_List.Clear();
+        Chktext.text = "Read Stamp";
+        ReadStamp();
         base.Start();
         track = GetComponent<TrackableBehaviour>();
         if (track)
@@ -29,7 +35,6 @@ public class GameManager : TtsForm, ITrackableEventHandler
             track.RegisterTrackableEventHandler(this);
         }
     }
-
     void Update()
     {
         if (Application.platform == RuntimePlatform.Android)
@@ -40,20 +45,20 @@ public class GameManager : TtsForm, ITrackableEventHandler
                 Application.Quit();
             }
         }
-        if (Input.touchCount>0 && SoundTrackCount == true) //인식 완료 + 터치
+        if (Input.touchCount > 0 && SoundTrackCount == true) //인식 완료 + 터치
         {
             touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began && base.touchOn == false) // 사운드 트랙 처음 실행했을 때
             {
-                Chktext.text = "터치 이벤트 실행" + touchOn.ToString() + "카운트 : " + base.mycount + "음악 큐~";
+                //Chktext.text = "터치 이벤트 실행" + touchOn.ToString() + "카운트 : " + base.mycount + "음악 큐~\n"+check;
                 base.FieldString = str;
                 base.OnSpeakClick();
                 base.touchOn = true;
                 Debug.Log("사운드 실행 후 info : " + str);
             }
-            else if(touch.phase == TouchPhase.Began && base.touchOn == true)
+            else if (touch.phase == TouchPhase.Began && base.touchOn == true)
             {
-                Chktext.text = "터치 이벤트 실행" + touchOn.ToString() + "카운트 : " + base.mycount + "음악 그만";
+                //Chktext.text = "터치 이벤트 실행" + touchOn.ToString() + "카운트 : " + base.mycount + "음악 그만\n"+check;
                 base.OnStopClick();
                 base.touchOn = false;
             }
@@ -68,26 +73,25 @@ public class GameManager : TtsForm, ITrackableEventHandler
         {
             ScriptEnable(true);
             //Chktext.text = this.name;
-            
-         
+
+
             filename = this.name;
             filename_idx = filename.IndexOf("-"); // 중복된 이미지파일명에서 -만 추출해가지구 앞에 이름으로 xml파일 검색할 수 있도록 해주는 코드
-            
-            
+
+
             if (filename_idx > 0) // 인덱스가 0보다 적으면 -가 파일명 사이에 있다는 것!
             {
-                filename = this.name.Substring(0, filename_idx);
+                filename = this.name.Substring(0, filename_idx); // 파일명에서 - 제거
                 Debug.Log("- 인덱스 번호 : " + filename_idx);
                 filename_idx = 0;
-                Debug.Log("파일명 : " + filename + "실제파일명 : "+this.name);
+                Debug.Log("파일명 : " + filename + "실제파일명 : " + this.name);
             }
             else
                 Debug.Log("xxxxxxxxxx -문자 발견되지않음");
-            
+
             //녹음파일 component 불러와서 실행하는 부분
             //var sound = GameObject.Find(this.name).GetComponent<JB_AudioPlayer>();
             //sound.SoundPlay();
-
             LoadXML(filename);
         }
         else
@@ -110,14 +114,14 @@ public class GameManager : TtsForm, ITrackableEventHandler
         //xmlDoc.AppendChild(xmlDeclaration);
         xmlDoc.LoadXml(txtAsset.text);
         Debug.Log("XML로드 완료!!");
-        
+
 
 
         XmlNodeList all_nodes = xmlDoc.SelectNodes("jb");
         XmlNodeList img_all_nodes = xmlDoc.SelectNodes("jb/img");
         foreach (XmlNode node in all_nodes)
         {
-            foreach(XmlNode img_node in img_all_nodes)
+            foreach (XmlNode img_node in img_all_nodes)
             {
                 IEnumerator ienum = img_node.GetEnumerator();
                 while (ienum.MoveNext())
@@ -146,13 +150,13 @@ public class GameManager : TtsForm, ITrackableEventHandler
                         str = node.SelectSingleNode("info").InnerText;
                         //str = str.Replace("<br>", "\n");
                         str = str.Replace("<br>", "");
-                        for(int i=0; i<str.Length; i += 27)
+                        for (int i = 0; i < str.Length; i += 27)
                         {
                             str = str.Insert(i, "\n\t");
                             Debug.Log("개행이 삽입되는 곳의 위치 : " + i);
                             i += 2;
                         }
-
+                        AddStamp(_fileName);
                         Chktext.text = "\tName : " + node.SelectSingleNode("name").InnerText + "\n\n\t작가 : " + node.SelectSingleNode("artist").InnerText;
 
                         //화면을 터치했을때 사운드 플레이 동작 실시하기 위해서 변수 삽입
@@ -168,6 +172,45 @@ public class GameManager : TtsForm, ITrackableEventHandler
 
         }
     }
+    void ReadStamp()
+    {
+        Stamp_List.Clear();
+        string line = "";// 한줄씩 입력받을 변수
+        FileStream Readfs = new FileStream(Application.persistentDataPath + "/ARculture" + "/Stamp.txt", FileMode.OpenOrCreate, FileAccess.Read);
+        StreamReader sr = new StreamReader(Readfs);
+        while ((line = sr.ReadLine()) != null)
+        {
+            Stamp_List.Add(line); // 모두 읽어서 리스트에 저장해둠. -> AddStamp 메소드에서 다루도록
+        }
+        sr.Close();
+        Readfs.Close();
 
-    
+    }
+    void AddStamp(string StampName)
+    {
+        //파일생성은 이미 완료되어있어야 함. Chktext 로드시 폴더&파일생성
+        //사진 인식할 때마다 계속 파일 켯다 끄는건 너무 cpu 소모를 불러일으키는가? 근데 실제로 렉을 부른다거나 하진 않음.
+        //먼저 파일을 전부 읽어서 파일이 또 존재하는지 체크하는게 필요하다.
+        int check = -1;
+        foreach(var obj in Stamp_List)
+        {
+            if (obj.IndexOf(StampName) >= 0)
+            {
+                check++;
+            }
+        }
+        if (check < 0)
+        {
+            FileStream fs = new FileStream(Application.persistentDataPath + "/ARculture" + "/Stamp.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            //경로 : 내 PC\Samsung Galaxy S7 edge\Phone\Android\data\com.AR.test\files
+            sw.WriteLine(StampName + "\n");
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            ReadStamp();
+        }
+    }
+
+
 }
