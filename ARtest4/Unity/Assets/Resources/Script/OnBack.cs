fd5e8d897299ebb8e9a1d6ddb0ebb5e75f0d0b61
minjoonee <jbjb3044@gmail.com>
2019-05-29 23:05:56 +0900

    뒤로가기
    onback.cs

---
+++ ARtest4/Unity/Assets/Resources/Script/OnBack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+            jo.Call("goBack");
+        }
+    }
+}
